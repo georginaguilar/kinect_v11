f3823e5b8912ca9912633413bdcd3ba677a736b7
georginaguilar <damara.aguilar.lopez@gmail.com>
2016-04-02 04:48:30 -0800

    Update April 2016
    Changes on GUI and other parameters , adapting DB, and adding structure
    of DB for futures proofs

---
+++ connetion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Microsoft.Samples.Kinect.FaceBasics
+{
+    class connetion
+    {
+        public MySqlConnection conection = new MySqlConnection("persistsecurityinfo=True;server=localhost;user=root;password=1234;database=kinect_exp2");
+
+        public void connect()
+        {
+            try
+            {
+                conection.Open();
+            }
+            catch (MySqlException ex)
+            {
+                string err = ex.ToString();
+            }
+        }
+        public void desconectar()
+        {
+            conection.Close();
+        }
+    }
+}
